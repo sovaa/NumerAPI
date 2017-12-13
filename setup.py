b4f45b8b8b76aa4182804fa1ca4b676ba27dd629
Oscar Eriksson <oscar@thenetcircle.com>
2017-12-13 12:31:48 +0800

    pypi preparations

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+numerapi_version = '0.2.0'
 
 setup(
     name="numerapi",
-    install_requires=["numpy", "flake8", "pylint", "requests"],
-    packages=["numerapi"]
+    version=numerapi_version,
+    description="Wrapper for the Numerai API",
+    url='https://github.com/numerai/NumerAPI',
+    license='Apache 2.0',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "requests",
+    ],
+    test_requires=[
+        "pytest",
+    ]
 )
