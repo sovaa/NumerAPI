e96372c1c818dbec0b65f2fa287965c52ff774bb
Oscar Eriksson <oscar@thenetcircle.com>
2017-12-13 11:39:23 +0800

    reverted pypi preparations to be submitted in separate PR

--- setup.py
+++ setup.py
@@ -1,21 +1,7 @@
-from setuptools import setup, find_packages
-
-numerapi_version = '0.3.0'
+from setuptools import setup
 
 setup(
     name="numerapi",
-    version=numerapi_version,
-    description="Wrapper for the Numerai API",
-    url='https://github.com/numerai/NumerAPI',
-    license='Apache 2',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "requests",
-        "zope.interface",
-    ],
-    test_requires=[
-        "pytest",
-    ]
+    install_requires=["numpy", "flake8", "pylint"],
+    packages=["numerapi"]
 )
